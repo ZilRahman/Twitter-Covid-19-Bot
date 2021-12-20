e4cb790b390a1f3022ab6b41c1e7a822fbad4164
ZilRahman <77643517+ZilRahman@users.noreply.github.com>
2021-12-20 12:30:46 -0700

    able to tweet
    updated permission settings to read and write

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
+require('dotenv').config()
+import TwitterApi from 'twitter-api-v2';
 export * from "./bot";
 
+
+const start = async () => {
+    // OAuth 1.0a (User context)
+    const userClient = new TwitterApi({
+        appKey: process.env.api_key as string,
+        appSecret: process.env.api_key_secret as string,
+        // Following access tokens are not required if you are
+        // at part 1 of user-auth process (ask for a request token)
+        // or if you want a app-only client (see below)
+        accessToken: process.env.access_token,
+        accessSecret: process.env.access_token_secret,
+    });
+
+
+    await userClient.v2.tweet('Hello, this is a test.');
+}
+
+start();
