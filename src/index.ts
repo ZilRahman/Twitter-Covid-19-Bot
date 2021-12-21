d8feb97b5a24bbb09616406d261a1d14f08307d5
ZilRahman <77643517+ZilRahman@users.noreply.github.com>
2021-12-21 15:01:42 -0700

    stream rules working

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,88 @@
 require('dotenv').config()
-import TwitterApi from 'twitter-api-v2';
+import TwitterApi, { ETwitterStreamEvent } from 'twitter-api-v2';
 export * from "./bot";
 
 
 const start = async () => {
     // OAuth 1.0a (User context)
-    const userClient = new TwitterApi({
-        appKey: process.env.api_key as string,
-        appSecret: process.env.api_key_secret as string,
-        // Following access tokens are not required if you are
-        // at part 1 of user-auth process (ask for a request token)
-        // or if you want a app-only client (see below)
-        accessToken: process.env.access_token,
-        accessSecret: process.env.access_token_secret,
+    // const userClient = new TwitterApi({
+    //     appKey: process.env.API_KEY as string,
+    //     appSecret: process.env.API_KEY_SECRET as string,
+    //     // Following access tokens are not required if you are
+    //     // at part 1 of user-auth process (ask for a request token)
+    //     // or if you want a app-only client (see below)
+    //     accessToken: process.env.ACCESS_TOKEN,
+    //     accessSecret: process.env.ACCESS_TOKEN_SECRET,
+    // });
+
+    const userClient = new TwitterApi(process.env.BEARER_TOKEN as string);
+
+
+    // add rules
+    const addedRules = await userClient.v2.updateStreamRules({
+        add: [
+            { 
+                value: 'covid is (fake OR a hoax OR a joke) lang:en ', 
+                tag: 'denial covid tweets',
+            }
+        ],
     });
 
+    // delete rules; rules must be deleted expicitly via id["string"] when not in use
+    const deleteRules = await userClient.v2.updateStreamRules({
+        delete: {
+            // deleted rules; tools and greetings rules (test rules)
+            ids : ['1473353750815264769', '1473354573100183554'],
+        },
+    })
 
-    await userClient.v2.tweet('Hello, this is a test.');
+    // console log current rules
+    const rules = await userClient.v2.streamRules();
+    console.log(rules.data.map( rules => rules.id ));
+
+
+
+    const stream = await userClient.v2.searchStream();
+
+// Awaits for a tweet
+    stream.on(
+        // Emitted when Node.js {response} emits a 'error' event (contains its payload).
+        ETwitterStreamEvent.ConnectionError,
+        err => console.log('Connection error!', err),
+    );
+
+    stream.on(
+        // Emitted when Node.js {response} is closed by remote or using .close().
+        ETwitterStreamEvent.ConnectionClosed,
+        () => console.log('Connection has been closed.'),
+    );
+
+    stream.on(
+        // Emitted when a Twitter payload (a tweet or not, given the endpoint).
+        ETwitterStreamEvent.Data,
+        eventData => console.log('Twitter has sent something:', eventData),
+    );
+
+    stream.on(
+        // Emitted when a Twitter sent a signal to maintain connection active
+        ETwitterStreamEvent.DataKeepAlive,
+        () => console.log('Twitter has a keep-alive packet.'),
+    );
+
+    // Enable reconnect feature
+    stream.autoReconnect = true;
+
+    // Be sure to close the stream where you don't want to consume data anymore from it
+    // stream.close();
+
+    // -- Alternative usage --
+
+    // You can also use async iterator to iterate over tweets!
+    // for await (const { data } of stream) {
+    // console.log('This is my tweet:', data);
+    // }
+
+    // await userClient.v2.tweet('Hello, this is a test. #2');
 }
 
 start();
